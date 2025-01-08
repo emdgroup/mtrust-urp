5665e29451470566f3e191ad764836295d2a2a41
Joshua Wellbrock <info@joshuawellbrock.com>
2025-01-08 09:15:58 +0100

    fix: remove adding commands to queue as they need to be wrapped in a device specific command wrapper: all methods in CmdWrapper now return the UrpCoreCommand

--- mtrust_urp_core/lib/src/chargin_station_cmd_wrapper.dart
+++ mtrust_urp_core/lib/src/chargin_station_cmd_wrapper.dart
@@ -5,24 +5,26 @@ import 'package:mtrust_urp_core/mtrust_urp_core.dart';
 class CharginStationCmdWrapper extends CmdWrapper {
   /// Creates a new instance of [CharginStationCmdWrapper]
   CharginStationCmdWrapper({
-    required ConnectionStrategy connectionStrategy,
+    required this.connectionStrategy,
     UrpDeviceIdentifier? target,
     UrpDeviceIdentifier? origin,
-  }) : super(
-          strategy: connectionStrategy,
-          target: target ??
-              UrpDeviceIdentifier(
-                deviceClass: UrpDeviceClass.urpStation,
-                deviceType: UrpDeviceType.urpPsu,
-              ),
-          origin: origin ??
-              UrpDeviceIdentifier(
-                deviceClass: UrpDeviceClass.urpHost,
-                deviceType: (Platform.isAndroid || Platform.isIOS)
-                    ? UrpDeviceType.urpMobile
-                    : UrpDeviceType.urpDesktop,
-              ),
+  }) : target = target ?? UrpDeviceIdentifier(
+          deviceClass: UrpDeviceClass.urpStation,
+          deviceType: UrpDeviceType.urpPsu,
+        ),
+        origin = origin ?? UrpDeviceIdentifier(
+          deviceClass: UrpDeviceClass.urpHost,
+          deviceType: (Platform.isAndroid || Platform.isIOS)
+              ? UrpDeviceType.urpMobile
+              : UrpDeviceType.urpDesktop,
         );
+    
+    /// The connection strategy to be used.
+    final ConnectionStrategy connectionStrategy;
+    /// The identifier of the target device.
+    final UrpDeviceIdentifier target;
+    /// The identifier of the origin device.
+    final UrpDeviceIdentifier origin;
 
   // TODO(JoshuaWellbrock): IMPLEMENT
 }
--- mtrust_urp_core/lib/src/command_wrapper.dart
+++ mtrust_urp_core/lib/src/command_wrapper.dart
@@ -1,279 +1,164 @@
-import 'dart:async';
-
 import 'package:flutter/foundation.dart';
 import 'package:mtrust_urp_core/mtrust_urp_core.dart';
 import 'package:mtrust_urp_types/wrapper.pb.dart' as wrapper;
 
-/// Wrapper for the commands
+/// Wrapper for the core commands, these commands need to be wrapped in a
+/// device specific Command Wrapper before they can be send to a device.
 class CmdWrapper extends ChangeNotifier {
-  /// Creates a new instance of [CmdWrapper]
-  CmdWrapper({
-    required this.strategy,
-    required this.target,
-    required this.origin,
-  }) {
-    strategy.onPing(ping);
-  }
 
-  /// The connection strategy
-  ConnectionStrategy strategy;
-
-  /// The target device
-  UrpDeviceIdentifier target;
-
-  /// The origin device
-  UrpDeviceIdentifier origin;
-
-  /// Pings the device.
-  Future<void> ping() async {
-    await strategy.addQueue(
-      UrpCoreCommand(
-        command: wrapper.UrpCommand.urpPing,
-      ).writeToBuffer(),
-      target,
-      origin,
+  /// Command to ping the device.
+  UrpCoreCommand ping() {
+    return UrpCoreCommand(
+      command: wrapper.UrpCommand.urpPing,
     );
   }
 
-  /// Info returns the device info.
-  Future<UrpDeviceInfo> info() async {
-    final res = await strategy.addQueue(
-      UrpCoreCommand(
-        command: wrapper.UrpCommand.urpGetInfo,
-      ).writeToBuffer(),
-      target,
-      origin,
-    );
-
-    if (!res.hasPayload()) {
-      throw Exception('Failed to get info');
-    }
-    return UrpDeviceInfo.fromBuffer(res.payload);
-  }
-
-  /// Returns the power state of the device.
-  Future<UrpPowerState> getPower() async {
-    final res = await strategy.addQueue(
-      UrpCoreCommand(
-        command: wrapper.UrpCommand.urpGetPower,
-      ).writeToBuffer(),
-      target,
-      origin,
-    );
-
-    if (!res.hasPayload()) {
-      throw Exception('Failed to get power state');
-    }
-    return UrpPowerState.fromBuffer(res.payload);
-  }
-
-  /// Sets the device name.
-  Future<void> setName(String name) async {
-    await strategy.addQueue(
-      UrpCoreCommand(
-        command: wrapper.UrpCommand.urpSetName,
-        setNameParameters: UrpSetNameParameters(name: name),
-      ).writeToBuffer(),
-      target,
-      origin,
+  /// Command to get the device info.
+  UrpCoreCommand info() {
+    return UrpCoreCommand(
+      command: wrapper.UrpCommand.urpGetInfo,
     );
   }
 
-  /// Gets the device name. Returns the result if successful.
-  /// Triggers an error if failed.
-  Future<UrpDeviceName> getName() async {
-    final res = await strategy.addQueue(
-      UrpCoreCommand(
-        command: wrapper.UrpCommand.urpGetName,
-      ).writeToBuffer(),
-      target,
-      origin,
-    );
-
-    if (!res.hasPayload()) {
-      throw Exception('Failed to get name');
-    }
-    return UrpDeviceName.fromBuffer(res.payload);
-  }
-
-  /// Pair a device.
-  Future<void> pair() async {
-    await strategy.addQueue(
-      UrpCoreCommand(
-        command: wrapper.UrpCommand.urpPair,
-      ).writeToBuffer(),
-      target,
-      origin,
+  /// Command to get the power state of the device.
+  UrpCoreCommand getPower() {
+    return UrpCoreCommand(
+      command: wrapper.UrpCommand.urpGetPower,
     );
   }
 
-  /// Unpair a device.
-  Future<void> unpair() async {
-    await strategy.addQueue(
-      UrpCoreCommand(
-        command: wrapper.UrpCommand.urpUnpair,
-      ).writeToBuffer(),
-      target,
-      origin,
+  /// Command to set the device name.
+  UrpCoreCommand setName(String name) {
+    return UrpCoreCommand(
+      command: wrapper.UrpCommand.urpSetName,
+      setNameParameters: UrpSetNameParameters(name: name),
     );
   }
 
-  /// Start an access point for the firmware update.
-  Future<UrpWifiState> startAP(String ssid, String apk) async {
-    final res = await strategy.addQueue(
-      UrpCoreCommand(
-        command: wrapper.UrpCommand.urpStartAp,
-        apParameters: UrpApParamters(ssid: ssid, password: apk),
-      ).writeToBuffer(),
-      target,
-      origin,
-    );
-    return UrpWifiState.fromBuffer(res.payload);
-  }
-
-  /// Stops the access point.
-  Future<void> stopAP() async {
-    await strategy.addQueue(
-      UrpCoreCommand(
-        command: wrapper.UrpCommand.urpStopAp,
-      ).writeToBuffer(),
-      target,
-      origin,
+  /// Command to get the device name.
+  UrpCoreCommand getName() {
+    return UrpCoreCommand(
+      command: wrapper.UrpCommand.urpGetName,
     );
   }
 
-  /// Connects to an access point.
-  Future<UrpWifiState> connectAP(String ssid, String apk) async {
-    final res = await strategy.addQueue(
-      UrpCoreCommand(
-        command: wrapper.UrpCommand.urpConnectAp,
-        apParameters: UrpApParamters(ssid: ssid, password: apk),
-      ).writeToBuffer(),
-      target,
-      origin,
-    );
-    return UrpWifiState.fromBuffer(res.payload);
-  }
-
-  /// Disconnects from an access point.
-  Future<void> disconnectAP() async {
-    await strategy.addQueue(
-      UrpCoreCommand(
-        command: wrapper.UrpCommand.urpDisconnectAp,
-      ).writeToBuffer(),
-      target,
-      origin,
+  /// Command to pair a device.
+  UrpCoreCommand pair() {
+    return UrpCoreCommand(
+      command: wrapper.UrpCommand.urpPair,
     );
   }
 
-  /// Starts DFU.
-  Future<void> startDFU() async {
-    await strategy.addQueue(
-      UrpCoreCommand(
-        command: wrapper.UrpCommand.urpStartDfu,
-      ).writeToBuffer(),
-      target,
-      origin,
+  /// Command to unpair a device.
+  UrpCoreCommand unpair() {
+    return UrpCoreCommand(
+      command: wrapper.UrpCommand.urpUnpair,
     );
   }
 
-  /// Stops DFU.
-  Future<void> stopDFU() async {
-    await strategy.addQueue(
-      UrpCoreCommand(
-        command: wrapper.UrpCommand.urpStopDfu,
-      ).writeToBuffer(),
-      target,
-      origin,
+  @Deprecated(
+    'This method is deprecated and will be removed in a future release',
+  )
+  /// Command to start an access point for the firmware update.
+  UrpCoreCommand startAP(String ssid, String apk) {
+    return UrpCoreCommand(
+      command: wrapper.UrpCommand.urpStartAp,
+      apParameters: UrpApParamters(ssid: ssid, password: apk),
     );
   }
 
-  /// Sleep put the device to sleep mode. It will disconnect from the device.
-  Future<void> sleep() async {
-    await strategy.addQueue(
-      UrpCoreCommand(
-        command: wrapper.UrpCommand.urpSleep,
-      ).writeToBuffer(),
-      target,
-      origin,
+  @Deprecated(
+    'This method is deprecated and will be removed in a future release',
+  )
+  /// Command to stop the access point.
+  UrpCoreCommand stopAP() {
+    return UrpCoreCommand(
+      command: wrapper.UrpCommand.urpStopAp,
     );
   }
 
-  /// Turns the device off. It will disconnect from the device.
-  Future<void> off() async {
-    await strategy.addQueue(
-      UrpCoreCommand(
-        command: wrapper.UrpCommand.urpOff,
-      ).writeToBuffer(),
-      target,
-      origin,
+  @Deprecated(
+    'This method is deprecated and will be removed in a future release',
+  )
+  /// Command to connect to an access point.
+  UrpCoreCommand connectAP(String ssid, String apk) {
+    return UrpCoreCommand(
+      command: wrapper.UrpCommand.urpConnectAp,
+      apParameters: UrpApParamters(ssid: ssid, password: apk),
     );
   }
 
-  /// Reboots the device. It will disconnect from the device.
-  Future<void> reboot() async {
-    await strategy.addQueue(
-      UrpCoreCommand(
-        command: wrapper.UrpCommand.urpReboot,
-      ).writeToBuffer(),
-      target,
-      origin,
+  @Deprecated(
+    'This method is deprecated and will be removed in a future release',
+  )
+  /// Command to disconnect from an access point.
+  UrpCoreCommand disconnectAP() {
+    return UrpCoreCommand(
+      command: wrapper.UrpCommand.urpDisconnectAp,
     );
   }
 
-  /// Prevents the device from going to sleep mode.
-  Future<void> stayAwake() async {
-    await strategy.addQueue(
-      UrpCoreCommand(
-        command: wrapper.UrpCommand.urpStayAwake,
-      ).writeToBuffer(),
-      target,
-      origin,
+  /// Command to start DFU.
+  UrpCoreCommand startDFU() {
+    return UrpCoreCommand(
+      command: wrapper.UrpCommand.urpStartDfu,
     );
   }
 
-  /// Gets the public key of the device. Returns the result if successful.
-  /// Triggers an error if failed.
-  Future<UrpPublicKey> getPublicKey() async {
-    final res = await strategy.addQueue(
-      UrpCoreCommand(
-        command: wrapper.UrpCommand.urpGetPublicKey,
-      ).writeToBuffer(),
-      target,
-      origin,
+  /// Command to stop DFU.
+  UrpCoreCommand stopDFU() {
+    return UrpCoreCommand(
+      command: wrapper.UrpCommand.urpStopDfu,
     );
-
-    if (!res.hasPayload()) {
-      throw Exception('Failed to get public key');
-    }
-    return UrpPublicKey.fromBuffer(res.payload);
   }
 
-  /// Gets the device id. Returns the result if successful.
-  /// Triggers an error if failed.
-  Future<UrpDeviceId> getDeviceId() async {
-    final res = await strategy.addQueue(
-      UrpCoreCommand(
-        command: wrapper.UrpCommand.urpGetDeviceId,
-      ).writeToBuffer(),
-      target,
-      origin,
+  /// Command to put the device to sleep mode. 
+  /// It will disconnect from the device.
+  UrpCoreCommand sleep() {
+    return UrpCoreCommand(
+      command: wrapper.UrpCommand.urpSleep,
     );
-
-    if (!res.hasPayload()) {
-      throw Exception('Failed to get device id');
-    }
-    return UrpDeviceId.fromBuffer(res.payload);
   }
 
-  /// Identify reader. Triggers the LED to identify the device.
-  Future<void> identify() async {
-    await strategy.addQueue(
-      UrpCoreCommand(
-        command: wrapper.UrpCommand.urpIdentify,
-      ).writeToBuffer(),
-      target,
-      origin,
+  /// Command to turn the device off. It will disconnect from the device.
+  UrpCoreCommand off() {
+    return UrpCoreCommand(
+      command: wrapper.UrpCommand.urpOff,
+    );
+  }
+
+  /// Command to reboot the device. It will disconnect from the device.
+  UrpCoreCommand reboot() {
+    return UrpCoreCommand(
+      command: wrapper.UrpCommand.urpReboot,
+    );
+  }
+
+  /// Command to prevent the device from going to sleep mode.
+  UrpCoreCommand stayAwake() {
+    return UrpCoreCommand(
+      command: wrapper.UrpCommand.urpStayAwake,
+    );
+  }
+
+  /// Command to get the public key of the device. 
+  UrpCoreCommand getPublicKey() {
+    return UrpCoreCommand(
+      command: wrapper.UrpCommand.urpGetPublicKey,
+    );
+  }
+
+  /// Command to get the device id
+  UrpCoreCommand getDeviceId() {
+    return UrpCoreCommand(
+      command: wrapper.UrpCommand.urpGetDeviceId,
+    );
+  }
+
+  /// Command to identify a reader. Triggers the LED to identify the device.
+  UrpCoreCommand identify() {
+    return UrpCoreCommand(
+      command: wrapper.UrpCommand.urpIdentify,
     );
   }
 }
