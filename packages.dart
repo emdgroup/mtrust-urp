d8b28cd95f3894787c6a9f973c83fc2d98ce335f
Yannick Stolle <yannick.stolle@merckgroup.com>
2025-01-14 13:44:39 +0100

    chore: consolidate scripts

--- packages.dart
+++ packages.dart
@@ -1,9 +1,38 @@
+/* Utility to work with all packages
+  
+  dart run packages.dart 
+    install - Install all packages dependencies
+    test - Run tests in all packages
+    analyze - Analyze all packages
+    set-deps [local] - Set all packages dependencies to local or hosted
+    update-urp-types - Update all packages dependencies to the latest version of mtrust_urp_types
+*/
 import 'dart:convert';
 import 'dart:io';
 
+import 'package:args/args.dart';
 import 'package:pub_semver/pub_semver.dart';
 import 'package:pubspec/pubspec.dart';
 import 'package:yaml_modify/yaml_modify.dart';
+
+List<String> changedPackagesOnly() {
+  final result = Process.runSync("git", "diff --name-only --cached".split(" "));
+
+  if (result.exitCode != 0) {
+    print("Error running git diff --name-only --cached");
+    exit(result.exitCode);
+  }
+
+  final changedPackages = <String>[];
+
+  LineSplitter.split(result.stdout as String).forEach((line) {
+    if (line.startsWith("mtrust_urp_")) {
+      changedPackages.add(line.split("/").first);
+    }
+  });
+
+  return changedPackages;
+}
 
 final subPackages = [
   "mtrust_urp_ble_strategy",
@@ -14,8 +43,45 @@ final subPackages = [
   "mtrust_urp_ui"
 ];
 
-void main(List<String> args) {
-  bool local = args.firstOrNull == "local";
+void runAll(String cmd, List<String> args,
+    {Set<String> exclude = const {}, bool changedOnly = false}) {
+  final packages = changedOnly ? changedPackagesOnly() : subPackages;
+
+  for (var subPackage in packages.where((e) => !exclude.contains(e))) {
+    print("> Running $cmd ${args.join(" ")} in $subPackage");
+    final result = Process.runSync(cmd, args, workingDirectory: subPackage);
+
+    if (result.exitCode != 0) {
+      print("Error running $cmd ${args.join(" ")} in $subPackage");
+
+      exit(result.exitCode);
+    }
+  }
+}
+
+void install(bool changedOnly) {
+  runAll("flutter", ["pub", "get"], changedOnly: changedOnly);
+}
+
+void test(bool changedOnly) {
+  runAll("flutter", ["test"], changedOnly: changedOnly);
+}
+
+void analyze(bool changedOnly) {
+  runAll("flutter", ["analyze"], changedOnly: changedOnly);
+}
+
+void updateUrpTypes() {
+  runAll("flutter", ["pub", "upgrade", "mtrust_urp_types"],
+      exclude: {"mtrust_urp_ui"});
+}
+
+void setDependencies(bool local) {
+  if (local) {
+    print("Setting all packages dependencies to local");
+  } else {
+    print("Setting all packages dependencies to hosted");
+  }
 
   final packageVersion =
       jsonDecode(File("package.json").readAsStringSync())["version"] as String;
@@ -56,6 +122,52 @@ void main(List<String> args) {
 
     json["version"] = packageVersion;
 
+    if (local) {
+      json["publish_to"] = "none";
+    } else {
+      json.remove("publish_to");
+    }
+
     pubSpecFile.writeAsStringSync(toYamlString(json));
   }
+}
+
+void main(List<String> args) {
+  var parser = ArgParser();
+
+  parser.addFlag("changed-only", abbr: "c", defaultsTo: false);
+
+  parser.addCommand("install");
+  parser.addCommand("test");
+  parser.addCommand("analyze");
+  final setDeps = parser.addCommand("set-deps");
+  setDeps.addOption("source", abbr: "s", allowed: ["local", "hosted"]);
+
+  parser.addCommand("update-urp-types");
+
+  var results = parser.parse(args);
+
+  if (results.command != null) {
+    switch (results.command!.name) {
+      case "install":
+        install(results.flag("changed-only"));
+        break;
+      case "test":
+        test(results.flag("changed-only"));
+        break;
+      case "analyze":
+        analyze(results.flag("changed-only"));
+        break;
+      case "set-deps":
+        setDependencies(results.command!["source"] == "local");
+        break;
+      case "update-urp-types":
+        updateUrpTypes();
+        break;
+    }
+    return;
+  }
+  print("Invalid command");
+
+  print(parser.usage);
 }
