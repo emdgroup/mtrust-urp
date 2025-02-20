062f221a114a13cddc8deba451d204fdc67421b7
Joshua Wellbrock <157679005+JoshuaWell@users.noreply.github.com>
2025-02-20 10:50:58 +0100

    feat: token (#7)
    * feat: token
    * fix: chunk ble messages
    * test: add API test
    * fix: device connector modal, add API_URL env variable
    * fix: device connector modal, add API_URL env variable
    
    ---------
    
    Co-authored-by: Joshua Wellbrock <info@joshuawellbrock.com>
    Co-authored-by: KochC <Christopher.Koch@merckgroup.com>

---
+++ mtrust_urp_core/lib/src/api_service.dart
@@ -0,0 +1,35 @@
+import 'dart:convert';
+
+import 'package:http/http.dart' as http;
+import 'package:mtrust_urp_core/mtrust_urp_core.dart';
+
+/// Service for API calls
+class ApiService {
+
+  /// M-Trust API URL
+  final url = const String.fromEnvironment('API_URL', defaultValue: 'https://api.mtrust.io');
+  
+  /// Fetch new [UrpSecureToken] from M-Trust API
+  Future<UrpSecureToken?> requestToken(
+    UrpSecureToken requestToken, 
+    UrpPublicKey publicKey,
+  ) async {
+    try {
+      final res = await http.post(
+        Uri.parse('$url/api/device/v1/device-tokens'),
+        headers: {
+          'Authorization': base64Encode(publicKey.value),
+          'Content-Type': 'application/octet-stream',
+        },
+        body: requestToken.writeToBuffer(),
+      );
+
+      final token = UrpSecureToken.fromBuffer(res.bodyBytes);
+      return token;
+    } catch (e) {
+      urpLogger.e('Failed to fetch token! $e');
+      return null;
+    }
+  }
+
+}
