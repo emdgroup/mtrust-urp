2b0c0f14ac062b2ef2d2b3dd10c570428fe0417f
Yannick <yannick.stolle@gmail.com>
2025-05-11 18:50:06 +0200

    chore: simplify CI using packages.dart script

--- packages.dart
+++ packages.dart
@@ -15,13 +15,15 @@ void main(List<String> args) {
   final runner = CommandRunner("packages", "Manage mtrust_urp packages")
     ..addCommand(SetDependenciesCmd())
     ..addCommand(UpdateUrpTypesCmd())
+    ..addCommand(CheckLicensesCmd())
     ..addCommand(InstallCmd())
     ..addCommand(TestCmd())
-    ..addCommand(AnalyzeCmd());
+    ..addCommand(AnalyzeCmd())
+    ..addCommand(IntlCmd());
 
   runner.run(args).catchError((error) {
     if (error is! UsageException) throw error;
-    print(error);
+    print("❗ $error");
     exit(64); // Exit code 64 indicates a usage error.
   });
 }
@@ -30,7 +32,7 @@ List<String> changedPackagesOnly() {
   final result = Process.runSync("git", "diff --name-only --cached".split(" "));
 
   if (result.exitCode != 0) {
-    print("Error running git diff --name-only --cached");
+    print("❌ Error running git diff --name-only --cached");
     exit(result.exitCode);
   }
 
@@ -51,20 +53,41 @@ final subPackages = [
   "mtrust_urp_usb_strategy",
   "mtrust_urp_virtual_strategy",
   "mtrust_urp_wifi_strategy",
-  "mtrust_urp_ui"
+  "mtrust_urp_ui",
+  "mtrust_urp_ui/example"
 ];
 
-void runAll(String cmd, List<String> args,
-    {Set<String> exclude = const {}, bool changedOnly = false}) {
+void runAll(
+  String cmd,
+  List<String> args, {
+  Set<String> exclude = const {},
+  bool changedOnly = false,
+  bool Function(PubSpec pubspec)? predicate,
+}) {
   final packages = changedOnly ? changedPackagesOnly() : subPackages;
 
   for (var subPackage in packages.where((e) => !exclude.contains(e))) {
-    print("> Running $cmd ${args.join(" ")} in $subPackage");
+    if (predicate != null) {
+      final pubspecFile = File("$subPackage/pubspec.yaml");
+      if (pubspecFile.existsSync()) {
+        final pubspec = PubSpec.fromYamlString(pubspecFile.readAsStringSync());
+        if (!predicate(pubspec)) {
+          print(">⏭️ Skipping $subPackage because of predicate");
+          continue;
+        }
+      } else {
+        print(">⏭️ Skipping $subPackage because pubspec.yaml not found");
+        continue;
+      }
+    }
+
+    print(">🚀 Running $cmd ${args.join(" ")} in $subPackage");
+
     final result = Process.runSync(cmd, args, workingDirectory: subPackage);
 
     if (result.exitCode != 0) {
-      print("Error running $cmd ${args.join(" ")} in $subPackage");
-
+      print("❌ Error running $cmd ${args.join(" ")} in $subPackage");
+      print("🪲 ${result.stderr}");
       exit(result.exitCode);
     }
   }
@@ -78,20 +101,47 @@ void test(bool changedOnly) {
   runAll("flutter", ["test"], changedOnly: changedOnly);
 }
 
+void intl(bool changedOnly) {
+  runAll(
+    "flutter",
+    ["gen-l10n"],
+    changedOnly: changedOnly,
+    predicate: (pubspec) {
+      return pubspec.unParsedYaml?["flutter"]?["generate"] == true;
+    },
+  );
+}
+
+void checkLicenses(bool changedOnly) {
+  Process.runSync("dart", ["pub", "global", "activate", "very_good_cli"]);
+  runAll(
+      "very_good",
+      [
+        "packages",
+        "check",
+        "licenses",
+        "--allowed=MIT,Apache-2.0,BSD-3-Clause,BSD-2-Clause"
+      ],
+      changedOnly: changedOnly);
+}
+
 void analyze(bool changedOnly) {
   runAll("flutter", ["analyze"], changedOnly: changedOnly);
 }
 
 void updateUrpTypes() {
-  runAll("flutter", ["pub", "upgrade", "mtrust_urp_types"],
-      exclude: {"mtrust_urp_ui"});
+  runAll(
+    "flutter",
+    ["pub", "upgrade", "mtrust_urp_types"],
+    exclude: {"mtrust_urp_ui"},
+  );
 }
 
 void setDependencies(bool local) {
   if (local) {
-    print("Setting all packages dependencies to local");
+    print("🔗 Setting all packages dependencies to local");
   } else {
-    print("Setting all packages dependencies to hosted");
+    print("🌐 Setting all packages dependencies to hosted");
   }
 
   final packageVersion =
@@ -158,7 +208,7 @@ class SetDependenciesCmd extends Command {
   @override
   Future<void> run() async {
     if (argResults?.option("source") == null) {
-      print("Please provide a source option --source=<local|hosted>");
+      print("⚠️ Please provide a source option --source=<local|hosted>");
       exit(1);
     }
 
@@ -227,5 +277,39 @@ class AnalyzeCmd extends Command {
   @override
   Future<void> run() async {
     analyze(argResults!.flag("changed-only"));
+  }
+}
+
+class CheckLicensesCmd extends Command {
+  @override
+  String get name => "check-licenses";
+
+  @override
+  String get description => "Check licenses in all packages";
+
+  CheckLicensesCmd() {
+    argParser.addFlag("changed-only", abbr: "c", defaultsTo: false);
+  }
+
+  @override
+  Future<void> run() async {
+    checkLicenses(argResults!.flag("changed-only"));
+  }
+}
+
+class IntlCmd extends Command {
+  @override
+  String get name => "intl";
+
+  @override
+  String get description => "Run flutter gen-l10n in all packages";
+
+  IntlCmd() {
+    argParser.addFlag("changed-only", abbr: "c", defaultsTo: false);
+  }
+
+  @override
+  Future<void> run() async {
+    intl(argResults!.flag("changed-only"));
   }
 }
