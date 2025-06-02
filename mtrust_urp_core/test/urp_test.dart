8d9e845746693a7d06a98b62e40a3393ba2b4b9c
Joshua Wellbrock <info@joshuawellbrock.com>
2025-06-02 10:46:42 +0200

    chore: add comments, update API Test

--- mtrust_urp_core/test/urp_test.dart
+++ mtrust_urp_core/test/urp_test.dart
@@ -1,27 +1,20 @@
-import 'dart:convert';
-
 import 'package:flutter_test/flutter_test.dart';
 import 'package:http/http.dart' as http;
 import 'package:mtrust_urp_core/mtrust_urp_core.dart';
 import 'package:mtrust_urp_core/src/api_service.dart';
 
 void main() {
+  // Test if the API is reachable and returns a valid response.
+  // As no token is provided, the API should return a 401.
   test('M-Trust API Test', () async {
-    final fakeToken = UrpSecureToken();
-    final fakePublicKey = UrpPublicKey();
     final apiService = ApiService();
 
     final response = await http.post(
       Uri.parse('${apiService.url}/api/device/v1/device-tokens'),
-      headers: {
-        'Authorization': base64Encode(fakePublicKey.value),
-        'Content-Type': 'application/octet-stream',
-      },
-      body: fakeToken.writeToBuffer(),
     );
 
-    if ([404, 500].contains(response.statusCode)) {
-      fail('FAILED: Status code for API request: ${response.statusCode}');
-    }
+    urpLogger.d('API Response: ${response.statusCode}');
+
+    expect(response.statusCode, 401);
   });
 }
