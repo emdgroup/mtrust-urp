b763d342520f875f2856c07579da4bb6200673b6
JoshuaWellbrock <info@joshuawellbrock.com>
2025-08-06 08:57:11 +0200

    fix: revert mtu size check as its causing issues on iOS

--- mtrust_urp_ble_strategy/lib/src/ble_strategy.dart
+++ mtrust_urp_ble_strategy/lib/src/ble_strategy.dart
@@ -233,9 +233,9 @@ class UrpBleStrategy extends ConnectionStrategy {
       await device.connect(
           timeout: const Duration(seconds: 5), autoConnect: false);
 
-      if(device.mtuNow < 512) {
-        throw BleMtuSizeException('MTU size is too small: ${device.mtuNow}. MTU size must be at least 512 bytes.');
-      }
+      // if(device.mtuNow < 512) {
+      //   throw BleMtuSizeException('MTU size is too small: ${device.mtuNow}. MTU size must be at least 512 bytes.');
+      // }
 
       _deviceSubscription = device.connectionState.listen(_deviceStateChanged);
 
